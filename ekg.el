8f4bc65e083868f46bba9966dee43e5eed3333ce
Qingshui Zheng <qingshuizheng@outlook.com>
2024-08-12 00:16:21 +0800

    [personal] Display extra titles as aliases

--- ekg.el
+++ ekg.el
@@ -853,7 +853,7 @@ FORMAT-STR controls how the time is formatted."
 (defun ekg-display-note-titled (note)
   "Return text of the title of NOTE."
   (if-let (titles (plist-get (ekg-note-properties note) :titled/title))
-      (propertize (concat (mapconcat #'identity titles "\n ") "\n")
+      (propertize (concat (mapconcat #'identity titles "\n[alias] ") "\n")
                   'face 'ekg-title)
     ""))
 
